757eb0278513e5e95a1882a1c3b42acbfe460a44
Lawrence <lawrence@acode.ninja>
2023-03-27 15:12:17 +0100

    Add virtual pixel matrix based display

--- app/display/Unicorn.py
+++ app/display/Unicorn.py
@@ -7,35 +7,62 @@ class UnicornDisplay:
         picounicorn.init()
         self.width = picounicorn.get_width()
         self.height = picounicorn.get_height()
+        self.pixels_current = [[[0, 0, 0]] * self.height for i in range(self.width)]
+        self.pixels_virtual = [[[0, 0, 0]] * self.height for i in range(self.width)]
+
+    def clear(self):
+        self.pixels_virtual = [[[0, 0, 0]] * self.height for i in range(self.width)]
+        return self
+
+    def render(self):
+        if self.pixels_current != self.pixels_virtual:
+            self.pixels_current = self.pixels_virtual
+            for x, row in enumerate(self.pixels_current):
+                for y, pixel in enumerate(row):
+                    r, b, g = pixel
+                    picounicorn.set_pixel(x, y, r, g, b)
+        return self
+
+    def set_pixel(self, x, y, r, g, b):
+        if 0 <= x < self.width and 0 <= y < self.height:
+            self.pixels_virtual[x][y] = [r, g, b]
+        return self
 
     def self_test(self):
-        brightness = 50
-        blinking = False
-        tick = 0
-
+        brightness = 20
+        print(f'pico unicorn pack width: {self.width}, height: {self.height}')
         while True:
-            if picounicorn.is_pressed(picounicorn.BUTTON_X) and brightness + 5 < 255:
-                brightness += 5
-            if picounicorn.is_pressed(picounicorn.BUTTON_Y) and brightness - 5 > 0:
-                brightness -= 5
-            if picounicorn.is_pressed(picounicorn.BUTTON_A):
-                blinking = True
-                tick = 1
-            if picounicorn.is_pressed(picounicorn.BUTTON_B):
-                blinking = False
-                tick = 1
+            # for x in range(self.width):
+            #     for y in range(self.height):
+            #         self\
+            #             .clear()\
+            #             .set_pixel(x, y, brightness, brightness, brightness)\
+            #             .render()
+            #         time.sleep(1.0 / 20)
+            #
+            # for x in range(self.width):
+            #     for y in range(self.height):
+            #         self\
+            #             .clear()\
+            #             .set_pixel(self.width - x, self.height - y, brightness, brightness, brightness)\
+            #             .render()
+            #         time.sleep(1.0 / 80)
 
-            if blinking and tick % 10 == 0:
-                for x in range(self.width):
-                    for y in range(self.height):
-                        picounicorn.set_pixel(x, y, 0, 0, 0)
-                tick = 0
-            else:
-                for x in range(self.width):
-                    for y in range(self.height):
-                        picounicorn.set_pixel(x, y, brightness, brightness, brightness)
-
-            tick += 1
-
-            time.sleep(1.0 / 60)
-
+            for x in range(self.width):
+                self.clear()
+                for y in range(self.height):
+                    if y <= 3:
+                        self.set_pixel(x, y, brightness, 0, 0)
+                        self.set_pixel(x + 1, y, 0, brightness, 0)
+                        self.set_pixel(x + 2, y, 0, 0, brightness)
+                self.render()
+                time.sleep(1.0 / 10)
+            for x in range(self.width):
+                self.clear()
+                for y in range(self.height):
+                    if y >= 3:
+                        self.set_pixel(self.width - x, y, brightness, 0, 0)
+                        self.set_pixel(self.width - x - 1, y, 0, brightness, 0)
+                        self.set_pixel(self.width - x - 2, y, 0, 0, brightness)
+                self.render()
+                time.sleep(1.0 / 10)
