e144bb5c2cc07a458a5c1c08bb435a8c04f933ef
Lawrence <lawrence@acode.ninja>
2023-03-28 15:17:49 +0100

    Add version command

--- cli/util/commands/__init__.py
+++ cli/util/commands/__init__.py
@@ -1,6 +1,7 @@
 from .command_build import CommandBuild
 from .command_init import CommandInit
 from .command_push import CommandPush
+from .command_version import CommandVersion
 from .command_wipe import CommandWipe
 
 available_commands = {
@@ -8,4 +9,5 @@ available_commands = {
     'init': CommandInit,
     'push': CommandPush,
     'wipe': CommandWipe,
+    'version': CommandVersion,
 }
