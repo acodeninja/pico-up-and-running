8cc81f85e118a2800386a416f9de88bdccb6f7de
Lawrence <lawrence@acode.ninja>
2023-03-29 08:46:55 +0100

    Better display of pico-up version

--- cli/util/commands/command_version.py
+++ cli/util/commands/command_version.py
@@ -9,6 +9,9 @@ class CommandVersion(CommandBase):
     @staticmethod
     def execute(configuration, arguments=None):
         output = os.popen('pip list').read()
+        version = 'dev'
         for item in output.split('\n'):
             if item.startswith('pico-up'):
-                cprint(item, 'blue')
+                version = item.split(' ')[-1]
+
+        cprint(f'pico-up version {version}', 'blue')
