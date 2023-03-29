df27563cd6de041ef2705da1215c262227fa8264
Lawrence <lawrence@acode.ninja>
2023-03-29 09:43:10 +0100

    Add prepare image command

--- pico_up/util/commands/__init__.py
+++ pico_up/util/commands/__init__.py
@@ -1,5 +1,6 @@
 from .command_build import CommandBuild
 from .command_init import CommandInit
+from .command_prepare_image import CommandPrepareImage
 from .command_push import CommandPush
 from .command_version import CommandVersion
 from .command_wipe import CommandWipe
@@ -7,6 +8,7 @@ from .command_wipe import CommandWipe
 available_commands = {
     'build': CommandBuild,
     'init': CommandInit,
+    'image': CommandPrepareImage,
     'push': CommandPush,
     'wipe': CommandWipe,
     'version': CommandVersion,
